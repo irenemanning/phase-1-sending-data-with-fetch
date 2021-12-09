20e324c5b5dbd0811573e474a187ff38e2159f64
Irene <irenemanningm@gmail.com>
2021-12-08 20:43:32 -0600

    commit tree 869d30eeff982e39b55d126feffb12aeb56ce7e4

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),    
+    })
+    .then(resp => resp.json())
+    .then(resp => {
+        document.body.innerHTML=resp['id']
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+        console.log(error.message)
+    })
+};
